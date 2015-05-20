192569c8de53bdcf702ffc5dae33d2ccb08e21be
Anton <tzat.anton@gmail.com>
2015-05-20 10:05:14 +0300

    Работя върху самата форма
    myjson вече има и атрибут задалжителен.

--- The Site/styles.js
+++ The Site/styles.js
@@ -1,27 +1,37 @@
-var json,success;
+var json = "undefined";
 
 function jsonp(j){
 	json=j;
 	console.log("got it!");
-	for (var a=0;a<json.array.length;a++){
-		$("div.buttons").append(
-		"<button class='default' id='"+json.array[a].name+"' name='"+json.array[a].name+"'>"+ json.array[a].name + "</button>"
-		);
-		$(".default").click(function(){
-			$("div.holder").animate({left:"-105%"});
-		});
-	}
-	
-	
-	}
+}
+
+function setEvent(a){
+	$("#" + a).click(function(){
+		console.log(a);
+		for (var b=0;b<json.array[a].form.length;b++){
+			$("div.form").append("<span class = 'desc'> "+json.array[a].form[b].desc+"</span>");	
+			if (json.array[a].form[b]==0) // TEXT
+				$("div.form").append("<input class = 'text' type = 'text' name='"+json.array[a].form[b].name+"'>");	
+			if (json.array[a].form[b]==1) // TELEPHONE
+				$("div.form").append("<input class = 'text' type = 'tel' name='"+json.array[a].form[b].name+"'>");	
+			if (json.array[a].form[b]==2) // TELEPHONE
+				$("div.form").append("<input class = 'text' type = 'tel' name='"+json.array[a].form[b].name+"'>");	
+		}
+		$("div.holder").animate({left:"-105%"});
+	});
+}
 
 jQuery(document).ready(
 function(){
-	jQuery.ajax({ 
-        type: 'GET',
-        url: 'http://anton.usr.me/responcive/myjson.json' ,
-        dataType: 'jsonp', 
-        success: function() {console.log("success");}
-	});
 	console.log("Hello jQuery!");
+	if(json!="undefined"){
+		for (var a=0;a<json.array.length;a++){
+		$("div.buttons").append(
+		"<button class='default' id='"+a+"' name='"+json.array[a].name+"'>"+ json.array[a].name + "</button>"
+		);
+		setEvent(a);
+		console.log(a);
+
+	}
+	}
 });
