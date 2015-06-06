9e7955ea65111fad08e12c329440636c1be45480
Anton <tzat.anton@gmail.com>
2015-06-06 13:24:43 +0300

    update

--- The Site/styles.js
+++ The Site/styles.js
@@ -11,12 +11,16 @@ function setEvent(a){
 		for (var b=0;b<json.array[a].form.length;b++){
 			$("div.form").append("<div class = 'formholder'></div>");	
 			$("div.formholder:last").append("<span class = 'desc'> "+json.array[a].form[b].desc+"</span>");	
-			if (json.array[a].form[b].type==0) // TEXT
-				$("div.formholder:last").append("<input class = 'text' type = 'text' name='"+json.array[a].form[b].name+"'>");	
-			if (json.array[a].form[b].type==1) // TELEPHONE
-				$("div.formholder:last").append("<input class = 'text' type = 'tel' name='"+json.array[a].form[b].name+"'>");	
-			if (json.array[a].form[b].type==2) // TELEPHONE
-				$("div.formholder:last").append("<input class = 'text' type = 'tel' name='"+json.array[a].form[b].name+"'>");	
+			if (json.array[a].form[b].type=="textinput") // TEXT
+				$("div.formholder:last").append("<input class = 'box' type = 'text' name='"+json.array[a].form[b].name+"'>");	
+			if (json.array[a].form[b].type=="telephone") // TELEPHONE
+				$("div.formholder:last").append("<input class = 'box' type = 'tel' name='"+json.array[a].form[b].name+"'>");	
+			if (json.array[a].form[b].type=="emailinput") // EMAIL
+				$("div.formholder:last").append("<input class = 'box' type = 'email' name='"+json.array[a].form[b].name+"'>");
+			if (json.array[a].form[b].type=="fileinput") // FILE
+				$("div.formholder:last").append("<input class = 'box' type = 'file' name='"+json.array[a].form[b].name+"'>");	
+			if (json.array[a].form[b].type=="checkbox") // CHECKBOX
+				$("div.formholder:last").append("<input class = 'box' type = 'checkbox' name='"+json.array[a].form[b].name+"'>");	
 				
 		}
 		$("div.holder").animate({left:"-105%"});
