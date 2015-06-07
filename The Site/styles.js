ec36d5e0fb2173edf884cfcd347d06006c783c9e
Anton <tzat.anton@gmail.com>
2015-06-07 09:36:10 +0300

    dfea

--- The Site/styles.js
+++ The Site/styles.js
@@ -16,14 +16,15 @@ function setEvent(a){
 		"<div style='height:80%;width:100%'><button class='back'>НАЗАД</button><img class='title' src="+json.array[a].img+"></img></div>"
 		);
 		$("button.back").click(function(){
-		$("div.holder").animate({left:"0"});
-		$("div.form").empty();
+			$("div.holder").animate({left:"0"});
+			$("div.form").empty();
 		});
+		$("div.form").append("<form id='frm' method='POST' action="+json.array[a].link+"></form>")
 		for (var b=0;b<json.array[a].form.length;b++){
 			var s1 = json.array[a].form[b].desc;
 			var s2 = json.array[a].form[b].must=="true"?'*':' ';
 			var s3 = s1.concat(s2);
-			$("div.form").append("<div class = 'formholder'></div>");	
+			$("#frm").append("<div class = 'formholder'></div>");
 			$("div.formholder:last").append("<span class = 'desc'> "+s3+"</span>");	
 			if (json.array[a].form[b].type=="textinput") // TEXT
 				$("div.formholder:last").append("<input class = 'box' type = 'text' name='"+json.array[a].form[b].name+"'>");	
@@ -38,9 +39,14 @@ function setEvent(a){
 			if (json.array[a].form[b].type=="dateinput") // DATE
 				$("div.formholder:last").append("<input class = 'box' type = 'date' name='"+json.array[a].form[b].name+"'>");
 			if (json.array[a].form[b].type=="urlinput") // URL
-				$("div.formholder:last").append("<input class = 'box' type = 'url' name='"+json.array[a].form[b].name+"'>");						
+				$("div.formholder:last").append("<input class = 'box' type = 'url' name='"+json.array[a].form[b].name+"'>");
+			if (json.array[a].form[b].type=="mapinput") // MAP
+				$("div.formholder:last").append(
+					"<div id='map-canvas'></div><input class = 'box' readonly='readonly' type = 'url' name='"+json.array[a].form[b].name+"'>"
+					
+				);						
 		}
-		$("div.form").append("<div class = 'formholder'></div>");	
+		$("#frm").append("<div class = 'formholder'></div>");	
 		$("div.formholder:last").append("<center><input class='submit' type='submit' value='Изпрати'></center>");
 		$("div.holder").animate({left:"-105%"});
 	});
